fb5e0ccf7e1c806db30d20a5279fc68a48e3fe2e
Joern Behrens <joern.behrens@uni-hamburg.de>
2021-06-18 12:48:17 +0200

    added copyright info to source

--- Hello.c
+++ Hello.c
@@ -3,12 +3,28 @@
  NAME:
 	Hello
 
- Description:
+ DESCRIPTION:
 	this is about the simplest program thinkable to demonstrate 
 	parallel execution with OpenMP
 
  VERSION(S):
 	1. original version		j. behrens	6/2021
+
+ COPYRIGHT INFORMATION:
+    Copyright (C) 2021  Jörn Behrens <joern.behrens@uni-hamburg.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 *****************************************************************/
 
